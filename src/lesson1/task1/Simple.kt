54341c4bce2d293ac128f01a301961cf12baabc4
xXTaTaPuHXx <aidargalim@inbox.ru>
2022-09-15 20:31:33 +0300

    first lesson complete(updated)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -122,11 +122,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double {
-    val firstyear = initial * (1 + percent / 100.0)
-    val secondyear = firstyear * (1 + percent / 100.0)
-    return secondyear * (1 + percent / 100.0)
-}
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+    ((initial * (1 + percent / 100.0)) * (1 + percent / 100.0)) * (1 + percent / 100.0)
 
 /**
  * Простая (2 балла)
